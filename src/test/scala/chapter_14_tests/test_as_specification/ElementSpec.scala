38e099ce365d09b33cc53380ca8ce70099941def
vladiksun85@gmail.com <nuhlqkbi85>
2018-11-22 20:02:09 +0300

    commit

--- src/test/scala/chapter_14_tests/test_as_specification/ElementSpec.scala
+++ src/test/scala/chapter_14_tests/test_as_specification/ElementSpec.scala
@@ -10,4 +10,15 @@ class ElementSpec extends FlatSpec with Matchers {
 		val ele = elem('x', 2, 3)
 		ele.width should be (2)
 	}
+
+    it should "have a height equal to the passed value" in {
+        val ele = elem('x', 2, 3)
+        ele.height should be (3)
+    }
+
+    it should "throw an IAE if passed a negative width" in {
+        an [IllegalArgumentException] should be thrownBy {
+            elem('x', -2, 3)
+        }
+    }
 }
