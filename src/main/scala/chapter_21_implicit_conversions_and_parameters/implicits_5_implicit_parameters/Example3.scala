0487ee0df01f6488eb62498e06ab3db3252c1bd1
Bondarchuk, Vladislav <VBondarchuk@nexign-systems.com>
2018-12-20 17:56:34 +0300

    +

---
+++ src/main/scala/chapter_21_implicit_conversions_and_parameters/implicits_5_implicit_parameters/Example3.scala
@@ -0,0 +1,51 @@
+package chapter_21_implicit_conversions_and_parameters.implicits_5_implicit_parameters
+
+object Example3 extends App {
+
+	def maxList1[T](elements: List[T])
+				  (implicit ordering: Ordering[T]): T =
+		elements match {
+			case List() =>
+				throw new IllegalArgumentException("empty list!")
+			case List(x) => x
+			case x :: rest =>
+				val maxRest = maxList1(rest) 	// (ordering) is implicit
+				if (ordering.gt(x, maxRest)) x 	// this ordering is still explicit
+				else maxRest
+		}
+
+	val max = maxList1(List(1,5,10,3))
+	println(max)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	def maxList2[T](elements: List[T])
+				  (implicit comparator: Ordering[T]): T =
+		elements match {
+			case List() =>
+				throw new IllegalArgumentException("empty list!")
+			case List(x) => x
+			case x :: rest =>
+				val maxRest = maxList2(rest)
+				if (implicitly[Ordering[T]].gt(x, maxRest)) x
+				else maxRest
+		}
+
+	val max2 = maxList2(List(1,5,10,3))
+	println(max2)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// use context bound
+	def maxList[T : Ordering](elements: List[T]): T =
+		elements match {
+			case List() =>
+				throw new IllegalArgumentException("empty list!")
+			case List(x) => x
+			case x :: rest =>
+				val maxRest = maxList(rest)
+				if (implicitly[Ordering[T]].gt(x, maxRest)) x
+				else maxRest
+		}
+
+	class NotOrdered(val x:Int)
+	maxList(List(new NotOrdered(1), new NotOrdered(2)))((x: NotOrdered, y: NotOrdered) => 1)
+}
