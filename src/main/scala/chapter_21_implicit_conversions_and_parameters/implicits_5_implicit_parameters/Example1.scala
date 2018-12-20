f7f4a99406fcc4b2a8cee8c178103f70018df828
Bondarchuk, Vladislav <VBondarchuk@nexign-systems.com>
2018-12-20 17:40:27 +0300

    +

---
+++ src/main/scala/chapter_21_implicit_conversions_and_parameters/implicits_5_implicit_parameters/Example1.scala
@@ -0,0 +1,29 @@
+package chapter_21_implicit_conversions_and_parameters.implicits_5_implicit_parameters
+
+object Example1 extends App {
+
+	class PreferredPrompt(val preference: String)
+	class PreferredDrink(val preference: String)
+
+	object Greeter {
+		def greet(name: String)(implicit prompt: PreferredPrompt,
+								drink: PreferredDrink) = {
+			println("Welcome, " + name + ". The system is ready.")
+			print("But while you work, ")
+			println("why not enjoy a cup of " + drink.preference + "?")
+			println(prompt.preference)
+		}
+	}
+
+	val bobsPrompt = new PreferredPrompt("relax> ")
+	val bobsDrink = new PreferredDrink("beer !!!!!!!!")
+	Greeter.greet("Bob")(bobsPrompt, bobsDrink) // supply explicitly
+
+	object JoesPrefs {
+		implicit val prompt = new PreferredPrompt("Yes, master> ")
+		implicit val drink = new PreferredDrink("tea")
+	}
+
+	import JoesPrefs._ 		// make an import to bring the implicit parameter to a scope
+	Greeter.greet("Joe")
+}
---
+++ src/main/scala/chapter_21_implicit_conversions_and_parameters/implicits_5_implicit_parameters/Example2.scala
@@ -0,0 +1,41 @@
+package chapter_21_implicit_conversions_and_parameters.implicits_5_implicit_parameters
+
+object Example2 extends App {
+
+	def maxListOrdering[T](elements: List[T])
+						  (ordering: Ordering[T]): T =
+		elements match {
+			case List() =>
+				throw new IllegalArgumentException("empty list!")
+			case List(x) => x
+			case x :: rest =>
+				val maxRest = maxListOrdering(rest)(ordering)
+				if (ordering.gt(x, maxRest)) x
+				else maxRest
+		}
+
+	// with implicit parameter
+	def maxListImpParm[T](elements: List[T])
+						 (implicit ordering: Ordering[T]): T =
+		elements match {
+			case List() =>
+				throw new IllegalArgumentException("empty list!")
+			case List(x) => x
+			case x :: rest =>
+				val maxRest = maxListImpParm(rest)(ordering)
+				if (ordering.gt(x, maxRest)) x
+				else maxRest
+		}
+
+	// implicit ordering is applied automatically because compiler can figure out that T is implements Ordering
+	maxListImpParm(List(1,5,10,3))
+	maxListImpParm(List(1.5, 5.2, 10.7, 3.14159))
+	maxListImpParm(List("one", "two", "three"))
+
+	// but for new not Ordering type we must provide it
+	class NotOrdered(val x:Int)
+
+	// will not compile
+//	maxListImpParm(List(new NotOrdered(1), new NotOrdered(2)))
+	maxListImpParm(List(new NotOrdered(1), new NotOrdered(2)))((x: NotOrdered, y: NotOrdered) => 1)
+}
