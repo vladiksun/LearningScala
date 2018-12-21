f14f74d51245e5bc45702c3a2b89ac9ee8c82995
Bondarchuk, Vladislav <VBondarchuk@nexign-systems.com>
2018-12-21 17:41:50 +0300

    +

---
+++ src/main/scala/chapter_23_for_expressions_revisited/ForExpr_5_TranslationToHigherOrderFunctions.scala
@@ -0,0 +1,9 @@
+package chapter_23_for_expressions_revisited
+
+object ForExpr_5_TranslationToHigherOrderFunctions {
+
+//	Every for expression can be expressed in terms of the three higher-order functions map,flatMap,
+//	and withFilter.
+
+	// Refer to book
+}
