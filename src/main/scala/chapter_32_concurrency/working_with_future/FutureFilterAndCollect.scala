b18d321ba5481f33acb54898b5e31d0ed1e56593
Bondarchuk, Vladislav <VBondarchuk@nexign-systems.com>
2019-03-01 18:59:45 +0300

    + scala with java

--- src/main/scala/chapter_32_concurrency/working_with_future/FutureFilterAndCollect.scala
+++ src/main/scala/chapter_32_concurrency/working_with_future/FutureFilterAndCollect.scala
@@ -1,7 +1,18 @@
 package chapter_32_concurrency.working_with_future
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object FutureFilterAndCollect extends App {
 
+	val fut = Future { 42 }
 
+	val valid = fut.filter(res => res > 0)
+	val invalid = fut.filter(res => res < 0)
+
+	Thread.sleep(2000)
+
+	println( valid.value )
+	println( invalid.value )
 
 }
